6438a35df929225befeb20ed7bce952b521aa036
jonathanhua99 <52897849+jonathanhua99@users.noreply.github.com>
2020-07-14 19:07:21 -0700

    Cleaning Account Creation/Login Pages (#4)
    -Can now enter trailing whitespace into the email form with trim()
    
    -Added gesture detector onto the login/create_account screens to allow for tapping away from the pop-up keyboard to switch focus
    
    -switched from buttons to flat buttons on "sign up" and "forgot your password" links, then commented that out and switched over to a richtext with a tap gesture recognizer. This makes it so that only the highlighted text can be pressed to redirect the page.
    
    -added textLabel for the createaccount page

--- lib/screens/create_account_screen.dart
+++ lib/screens/create_account_screen.dart
@@ -12,6 +12,7 @@ class CreateAccount extends StatefulWidget {
 
 class _CreateAccountState extends State<CreateAccount> {
   final AuthService _auth = AuthService();
+  //final formKey = GlobalKey<FormState>();
 
   String email;
   String password;
@@ -31,78 +32,90 @@ class _CreateAccountState extends State<CreateAccount> {
           ),
           title: Text("Create an account"),
         ),
-        body: Container(
-          color: Colors.white,
-          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-          child: Form(
-              child: Column(
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              TextFormField(
-                  decoration: InputDecoration(hintText: "Email"),
-                  onChanged: (val) {
-                    setState(() => email = val);
-                  }),
-              Text(errorEmail, style: TextStyle(color: Colors.red)),
-              SizedBox(height: 20.0),
-              TextFormField(
-                  decoration: InputDecoration(hintText: "Username"),
-                  onChanged: (val) {
-                    setState(() => username = val);
-                  }),
-              SizedBox(height: 20.0),
-              Text(errorUsername, style: TextStyle(color: Colors.red)),
-              TextFormField(
-                  decoration: InputDecoration(hintText: "Password"),
-                  obscureText: true,
-                  onChanged: (val) {
-                    setState(() => password = val);
+        body: new GestureDetector(
+          onTap: () {
+            FocusScope.of(context).requestFocus(new FocusNode());
+          },
+          child: Container(
+            color: Colors.white,
+            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+            child: Form(
+                child: Column(
+              children: <Widget>[
+                SizedBox(height: 20.0),
+                TextFormField(
+                    decoration:
+                        InputDecoration(labelText: "Email", hintText: "Email"),
+                    onChanged: (val) {
+                      setState(() => email = val.trim());
+                    }),
+                Text(errorEmail, style: TextStyle(color: Colors.red)),
+                SizedBox(height: 20.0),
+                TextFormField(
+                    decoration: InputDecoration(
+                        labelText: "Username", hintText: "Username"),
+                    onChanged: (val) {
+                      setState(() => username = val);
+                    }),
+                SizedBox(height: 20.0),
+                Text(errorUsername, style: TextStyle(color: Colors.red)),
+                TextFormField(
+                    decoration: InputDecoration(
+                        labelText: "Password", hintText: "Password"),
+                    obscureText: true,
+                    onChanged: (val) {
+                      setState(() => password = val);
 
-                    ///THIS PASSWORD VALIDATOR DOES NOT WORK YET
-                    var passValidateString = TextValidator(input: password);
-                    if (!passValidateString.isPasswordValid()) {
-                      setState(() => errorPassword =
-                          "Your password does not meet the requirements.");
-                    } else {
-                      setState(() => errorPassword = "");
-                    }
-                  }),
-              Text(errorPassword, style: TextStyle(color: Colors.red)),
-              SizedBox(height: 20.0),
-              RaisedButton(
-                color: Colors.lightBlue,
-                child: Text("Sign up", style: TextStyle(color: Colors.white)),
-                onPressed: () async {
-                  dynamic result =
-                      await _auth.createNewUser(email, username, password);
-                  if (result == null) {
-                    setState(() => errorEmail =
-                        // THIS IS NOT WORKING YET
-                        "An account under this email already exists.");
-                  } else {
-                    await showDialog(
-                        context: context,
-                        child: CupertinoAlertDialog(
-                          title: Text("Success!"),
-                          content: Text("Welcome to the app " +
-                              username +
-                              ", your account has been successfully created."),
-                          actions: <Widget>[
-                            CupertinoDialogAction(
-                              isDefaultAction: true,
-                              onPressed: (() =>
-                                  Navigator.of(context, rootNavigator: true)
-                                      .pop('dialog')),
-                              child: Text("OK"),
-                            )
-                          ],
-                        ));
-                    Navigator.pushReplacementNamed(context, LoginScreen.id);
-                  }
-                },
-              )
-            ],
-          )),
+                      ///THIS PASSWORD VALIDATOR DOES NOT WORK YET
+                      var passValidateString = TextValidator(input: password);
+                      if (!passValidateString.isPasswordValid()) {
+                        setState(() => errorPassword =
+                            "Your password does not meet the requirements.");
+                      } else {
+                        setState(() => errorPassword = "");
+                      }
+                    }),
+                Text(errorPassword, style: TextStyle(color: Colors.red)),
+                SizedBox(height: 20.0),
+                ButtonTheme(
+                  minWidth: 250.0,
+                  child: RaisedButton(
+                    color: Colors.lightBlue,
+                    child:
+                        Text("Sign up", style: TextStyle(color: Colors.white)),
+                    onPressed: () async {
+                      dynamic result =
+                          await _auth.createNewUser(email, username, password);
+                      if (result == null) {
+                        setState(() => errorEmail =
+                            // THIS IS NOT WORKING YET
+                            "An account under this email already exists.");
+                      } else {
+                        await showDialog(
+                            context: context,
+                            child: CupertinoAlertDialog(
+                              title: Text("Success!"),
+                              content: Text("Welcome to the app " +
+                                  username +
+                                  ", your account has been successfully created."),
+                              actions: <Widget>[
+                                CupertinoDialogAction(
+                                  isDefaultAction: true,
+                                  onPressed: (() =>
+                                      Navigator.of(context, rootNavigator: true)
+                                          .pop('dialog')),
+                                  child: Text("OK"),
+                                )
+                              ],
+                            ));
+                        Navigator.pushReplacementNamed(context, LoginScreen.id);
+                      }
+                    },
+                  ),
+                )
+              ],
+            )),
+          ),
         ));
   }
 }
