bb76886c4b1a134b70f3420398b7d67ce1ff8b4d
jonathanhua99 <jonathanhua99@gmail.com>
2020-07-14 19:06:27 -0700

    Cleaning Account Creation/Login Pages
    -Can now enter trailing whitespace into the email form with trim()
    
    -Added gesture detector onto the login/create_account screens to allow for tapping away from the pop-up keyboard to switch focus
    
    -switched from buttons to flat buttons on "sign up" and "forgot your password" links, then commented that out and switched over to a richtext with a tap gesture recognizer. This makes it so that only the highlighted text can be pressed to redirect the page.
    
    -added textLabel for the createaccount page

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cafe_app/screens/create_account_screen.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import '../services/auth.dart';
 import '../screens/home_screen.dart';
@@ -23,65 +24,98 @@ class _LoginScreenState extends State<LoginScreen> {
           title: Text("Login"),
           backgroundColor: Colors.lightBlue,
         ),
-        body: Container(
-            color: Colors.white,
-            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-            child: Form(
-                child: Column(
-              children: <Widget>[
-                SizedBox(height: 20.0),
-                TextFormField(
+        body: new GestureDetector(
+          onTap: () {
+            FocusScope.of(context).requestFocus(new FocusNode());
+          },
+          child: Container(
+              color: Colors.white,
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              child: Form(
+                  child: Column(
+                children: <Widget>[
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                      decoration: InputDecoration(
+                        helperText: "Email",
+                      ),
+                      onChanged: (val) {
+                        setState(() => email = val);
+                      }),
+                  SizedBox(height: 20.0),
+                  TextFormField(
                     decoration: InputDecoration(
-                      helperText: "Email",
+                      helperText: "Password",
                     ),
+                    obscureText: true,
                     onChanged: (val) {
-                      setState(() => email = val);
-                    }),
-                SizedBox(height: 20.0),
-                TextFormField(
-                  decoration: InputDecoration(
-                    helperText: "Password",
+                      setState(() => password = val);
+                    },
                   ),
-                  obscureText: true,
-                  onChanged: (val) {
-                    setState(() => password = val);
-                  },
-                ),
-                Text(error, style: TextStyle(color: Colors.red)),
-                SizedBox(height: 20.0),
-                RaisedButton(
-                    color: Colors.lightBlue,
-                    child:
-                        Text("Sign in", style: TextStyle(color: Colors.white)),
-                    onPressed: () async {
-                      dynamic result =
-                          await _auth.signInExistingUser(email, password);
-                      if (result == null) {
-                        setState(() => error = "Invalid user info. Try again.");
-                      } else {
-                        setState(() => error = "User information is valid.");
-                        Navigator.pushReplacementNamed(context, HomeScreen.id);
-                      }
-                    }),
-                Row(
-                  children: <Widget>[
-                    Padding(
+                  Text(error, style: TextStyle(color: Colors.red)),
+                  SizedBox(height: 20.0),
+                  ButtonTheme(
+                    minWidth: 400.0,
+                    height: 40.0,
+                    child: RaisedButton(
+                        color: Colors.lightBlue,
+                        child: Text("Sign in",
+                            style: TextStyle(color: Colors.white)),
+                        onPressed: () async {
+                          dynamic result =
+                              await _auth.signInExistingUser(email, password);
+                          if (result == null) {
+                            setState(
+                                () => error = "Invalid user info. Try again.");
+                          } else {
+                            Navigator.pushReplacementNamed(
+                                context, HomeScreen.id);
+                          }
+                        }),
+                  ),
+                  Padding(padding: EdgeInsets.symmetric(vertical: 15.0)),
+                  RichText(
+                    text: TextSpan(children: <TextSpan>[
+                      TextSpan(
+                          text: "Forgot your password?",
+                          style: TextStyle(color: Colors.blue),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap =
+                                () => print("go to forgot password screen"))
+                    ]),
+                  ),
+                  Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: RaisedButton(
-                          child: Text("Sign up"),
-                          onPressed: (() => Navigator.pushReplacementNamed(
-                              context, CreateAccount.id))),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: RaisedButton(
-                          child: Text("Forgot password?"),
-                          onPressed: (() => print("FORGOT PASSWORD PRESSED"))),
-                    )
-                  ],
-                ),
-                SizedBox(height: 20.0),
-              ],
-            ))));
+                      child: new Container(
+                          margin: const EdgeInsets.only(left: 5, right: 5),
+                          child: Divider(
+                            color: Colors.black,
+                            height: 36,
+                          ))),
+/*                   FlatButton(
+                      child: Text("Don't have an account? Sign up here."),
+                      onPressed: (() => Navigator.pushReplacementNamed(
+                          context, CreateAccount.id))), */
+                  RichText(
+                    text: TextSpan(children: <TextSpan>[
+                      TextSpan(
+                          text: "Don't have an account? ",
+                          style: TextStyle(color: Colors.black)),
+                      TextSpan(
+                          text: "Sign up here.",
+                          style: TextStyle(color: Colors.blue),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () => Navigator.pushReplacementNamed(
+                                context, CreateAccount.id)),
+                    ]),
+                  ),
+                  Padding(padding: EdgeInsets.symmetric(vertical: 15.0)),
+/*                  FlatButton(
+                      child: Text("Forgot password?",
+                          style: TextStyle(color: Colors.blue)),
+                      onPressed: (() => print("FORGOT PASSWORD PRESSED"))), */
+                ],
+              ))),
+        ));
   }
 }
